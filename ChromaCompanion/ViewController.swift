1a02520a2b3539b717335fb9e7884161d6efef1a
mxu24 <maggiex@uchicago.edu>
2024-05-15 13:22:41 -0500

    Initial commit

---
+++ ChromaCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChromaCompanion
+//
+//  Created by Main on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
