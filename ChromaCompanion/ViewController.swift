3c67afc9d163c74c49dc43c08ecb188208c24c0b
mxu24 <maggiex@uchicago.edu>
2024-05-18 13:29:35 -0500

    working camera + tabs

--- ChromaCompanion/ViewController.swift
+++ ChromaCompanion/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  ChromaCompanion
-//
-//  Created by Main on 5/15/24.
-//
+// With guidance from demo by iOS Academy on YouTube
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
+    
+    @IBAction func didTapButton() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
 
+extension ViewController: UIImagePickerControllerDelegate,
+                          UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as?
+                UIImage else {
+            return
+        }
+        imageView.image = image
+    }
 
 }
 
