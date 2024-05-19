31e68f7f59c4f93c9157c1b1285754d2ed92b03c
Kathy Yao <kathyyao06@gmail.com>
2024-05-19 11:48:56 -0500

    podfile + uiimagecolors

---
+++ Pods/Target Support Files/Pods-ChromaCompanion-ChromaCompanionUITests/Pods-ChromaCompanion-ChromaCompanionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChromaCompanion_ChromaCompanionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChromaCompanion_ChromaCompanionUITests
+@end
